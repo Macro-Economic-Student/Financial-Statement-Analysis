50b7ae18cde8cca8ee78eaa836714590bcb8268f
tancos02 <paulussiahaan02@gmail.com>
2025-09-04 15:31:54 +0700

    Add fitur aset in data
    Add data from Asset Financial Statement and combine it with other features

--- data/import_data.py
+++ data/import_data.py
@@ -5,19 +5,48 @@ def import_rasio() -> pd.DataFrame:
     """
     Import all Rasio data into one dataframe
     """
+
+    exclude_cols = [
+        'posisi',
+        'company_name',
+        'kbmi_type',
+        'year',
+        'quarter',
+        'year_quarter',
+        'company_date',
+    ]
+    
     # Path to this file's folder ("data")
     base_path = Path(__file__).parent
 
     # Build absolute paths to the Excel files
     file1 = base_path / "summarized rasio - KBMI 1.xlsx"
     file4 = base_path / "summarized rasio - KBMI 4.xlsx"
+    file_aset_4 = base_path / "summarized fitur aset - KBMI 4.xlsx"
 
+    # Read all data files
     df_kbmi_1 = pd.read_excel(file1)
     df_kbmi_1 = df_kbmi_1.drop(columns=["sort_key"], errors="ignore")
 
     df_kbmi_4 = pd.read_excel(file4)
 
+    df_aset_4 = pd.read_excel(file_aset_4)
+
+    # Combining all dataframes
     df = pd.concat([df_kbmi_1, df_kbmi_4], axis=0, join="outer", ignore_index=True)
+
+    df = pd.merge(
+        df,
+        df_aset_4,
+        on=['posisi', 'company_name'],
+        how='left',
+        suffixes=("", "_df2")
+    )
+
+    # drop only the df2 versions of excluded columns
+    drop_cols = [f"{c}_df2" for c in exclude_cols if f"{c}_df2" in df.columns]
+    df = df.drop(columns=drop_cols, errors="ignore")
+
     return df
 
 def import_fitur_rasio() -> list :
@@ -32,6 +61,15 @@ def import_fitur_rasio() -> list :
         'biaya_operasional_terhadap_pendapatan_operasional',
         'loan_to_deposit_ratio',
         'posisi_devisa_neto_(pdn)_secara_keseluruhan',
+
+        # Fitur dari Aset
+        'penempat_pada_bank_lain_per_total_aset',
+        'tagihan_spot_derivatif_forward_per_total_aset',
+        'surat_berharga_yang_dimiliki_per_total_aset',
+        'reverse_repo_per_total_aset',
+        'tagihan_akseptasi_per_total_aset',
+        'kyd_dan_pembiayaan_syariah_per_total_aset',
+        'ckpn_aset_keuangan_per_kyd_dan_pembiayaan_syariah'
     ]
 
     return(fitur_rasio)
@@ -48,6 +86,15 @@ def import_dictionary_rasio() -> dict :
         'biaya_operasional_terhadap_pendapatan_operasional': 'BOPO',
         'loan_to_deposit_ratio': 'LDR',
         'posisi_devisa_neto_(pdn)_secara_keseluruhan': 'PDN',
+
+        # Fitur dari Aset
+        'penempat_pada_bank_lain_per_total_aset' : 'Penempatan pada Bank Lain per Total Aset',
+        'tagihan_spot_derivatif_forward_per_total_aset' : 'Tagihan Spot Derivatif Forward per Total Aset',
+        'surat_berharga_yang_dimiliki_per_total_aset' : 'Surat Berharga yang Dimiliki per Total Aset',
+        'reverse_repo_per_total_aset' : 'Reverse Repo per Total Aset',
+        'tagihan_akseptasi_per_total_aset' : 'Tagihan Akseptasi per Total Aset',
+        'kyd_dan_pembiayaan_syariah_per_total_aset' : 'KYD dan Pembiayaan Syariah per Total Aset',
+        'ckpn_aset_keuangan_per_kyd_dan_pembiayaan_syariah' : 'CKPN Aset Keuangan per KYD dan Pembiayaan Syariah',
     }
 
     return(dict_rasio)
